156176e362f041fdbaca945ececad20b1b6ced30
dostonbokhodirov <boxodirov.doston@gmail.com>
2022-02-23 14:48:34 +0500

    initial commit

---
+++ src/main/java/uz/doston/springcrm/SpringCrmApplication.java
@@ -0,0 +1,13 @@
+package uz.doston.springcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/doston/springcrm/SpringCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.doston.springcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
