e6633ad36df82559e8fef40d106a8204872e9ff2
dostonbokhodirov <boxodirov.doston@gmail.com>
2022-02-26 12:24:00 +0500

    updated config

--- src/main/java/uz/doston/springcrm/config/security/CustomUserDetailsService.java
+++ src/main/java/uz/doston/springcrm/config/security/CustomUserDetailsService.java
@@ -1,27 +1,27 @@
-package uz.doston.springcrm.config.security;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import uz.doston.springcrm.entity.auth.AuthUser;
-import uz.doston.springcrm.repository.auth.AuthUserRepository;
-
-import java.util.Optional;
-
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-
-    public CustomUserDetailsService(AuthUserRepository authUserRepository) {
-        this.authUserRepository = authUserRepository;
-    }
-
-    private final AuthUserRepository authUserRepository;
-
-    @Override
-    public MyUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        AuthUser authUser = authUserRepository.findByUsername(username);
-        return new MyUserDetails(authUser);
-    }
-}
+//package uz.doston.springcrm.config.security;
+//
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//import uz.doston.springcrm.entity.auth.AuthUser;
+//import uz.doston.springcrm.repository.auth.AuthUserRepository;
+//
+//import java.util.Optional;
+//
+//@Service
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    public CustomUserDetailsService(AuthUserRepository authUserRepository) {
+//        this.authUserRepository = authUserRepository;
+//    }
+//
+//    private final AuthUserRepository authUserRepository;
+//
+//    @Override
+//    public MyUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        AuthUser authUser = authUserRepository.findByUsername(username);
+//        return new MyUserDetails(authUser);
+//    }
+//}
